9da7e8a0b07884f772efd9bd58ddb4a3fddb8e48
Filip Stefanovski <filipst@lnxfilipst.se.axis.com>
2020-11-30 12:55:57 +0100

    patch for images failing to fetch

--- Scraping.py
+++ Scraping.py
@@ -1,10 +1,12 @@
 import os
 import requests
+import urllib.request
 import json
 # pip3 install requests 
 # pip install jsonlib
 # sudo apt-get install jp2a 
 thisweek = requests.get('https://vote.fredagskakan.se/thisweek').json()
 url = thisweek["URL"]
-os.system("jp2a --colors " + url)
+urllib.request.urlretrieve(url, "/tmp/veckanskaka.jpg")
+os.system("jp2a --colors /tmp/veckanskaka.jpg")
 print ("Veckans kaka är",str(thisweek["Kaka"]))
